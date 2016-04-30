c197a95f20805ad94df5ca8a1bcbd399d02552ba
khajaasmath786 <mdkhajaasmath@gmail.com>
2016-04-30 14:42:30 -0500

    commit

---
+++ SparkPoc/src/main/scala/com/mcd/sparksql/datahub/CalDt.scala
@@ -0,0 +1,3 @@
+package com.mcd.sparksql.datahub
+
+case class CalDt (cal_dt:String,  yr_nu:String,  mo_nu:String  ,  mo_dy_nu:String,  cal_qtr_id_nu:String,  wk_end_sun_beg_dt:String,  wk_ds:String,  dy_of_cal_wk_ds:String,  cal_ly_dy_dt:String,  cal_ly_dt:String)
--- SparkPoc/src/main/scala/com/mcd/sparksql/datahub/CreateDatahubFiles.scala
+++ SparkPoc/src/main/scala/com/mcd/sparksql/datahub/CreateDatahubFiles.scala
@@ -7,16 +7,20 @@ import org.apache.spark.SparkConf
 import com.mcd.sparksql.datahub._
 import com.mcd.sparksql.util._
 import org.apache.spark.sql._
+import com.mcd.sparksql.datahub._
 
 object CreateDatahubFiles {
+
   def main(args: Array[String]): Unit = {
 
     val conf = DaasUtil.getJobConf("Generate Datahub With Spark SQL Dataframe", "local[2]", "1g", "1g");
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
-    //Defining Input Paths : Should be parameterized
-    val inputPath = "datahub/data,datahub/data2"
+    // ***************************** You can use below code directly ********************************************************************************************************************
+
+    /*//Defining Input Paths : Should be parameterized
+    val datahubInputPath = "datahub/data,datahub/data2"
 
     //Getting Schema of Datahub through Struct Type instead of Case Classes
     val customDatahubSchema = DataHubUtil.getSchemaforDatahub();
@@ -25,8 +29,8 @@ object CreateDatahubFiles {
     val datahubDataFrame = sqlContext.load(
       "com.databricks.spark.csv",
       schema = customDatahubSchema,
-      Map("path" -> inputPath, "header" -> "true", "delimiter" -> "\t"))
-      
+      Map("path" -> datahubInputPath, "header" -> "true", "delimiter" -> "\t")).cache()
+
     //Regsitering DF as temp table
     datahubDataFrame.registerTempTable("tld_datahub")
 
@@ -34,5 +38,65 @@ object CreateDatahubFiles {
     // results.save("newcars.csv", "com.databricks.spark.csv")
     results.map(t => "Posbusndt: " + t(0)).collect().foreach(println)
 
+    // this is used to implicitly convert an RDD to a DataFrame. (import sqlContext.implicits._)
+    import sqlContext.implicits._
+
+    //Defining Input Paths : Should be parameterized
+    val calDtInputPath = "datahub/caldt"
+    val calDt = sc.textFile("datahub/caldt/CAL_DT.txt").map(_.split("\t")).map(p => CalDt(p(0), p(1), p(2), p(3), p(4), p(5), p(6), p(7), p(8), p(9))).toDF()
+
+    val calDtDF = calDt.toDF()
+    calDtDF.registerTempTable("CAL_DT")
+
+    // SQL statements can be run by using the sql methods provided by sqlContext.
+    val calDate = sqlContext.sql("SELECT cal_dt from CAL_DT")*/
+
+    // ***************************** You can use above code directly ********************************************************************************************************************
+
+    
+    val datahubInputPath = "datahub/data,datahub/data2"
+    val datahubTableName="tld_datahub";
+    val datahubDataFrame = getDataHubDF(sc, sqlContext, datahubInputPath, datahubTableName)
+    datahubDataFrame.registerTempTable(datahubTableName)
+    val results = sqlContext.sql("SELECT  posbusndt,pos_itm_lvl_nu,last_updt_ts FROM tld_datahub")
+    // results.save("newcars.csv", "com.databricks.spark.csv")
+    results.map(t => "Posbusndt: " + t(0)).collect().foreach(println)
+    
+    
+    val calDtInputPath = "datahub/caldt"
+    val calDTableName="CAL_DT";
+    val calDtDataFrame = getCalDtDF(sc, sqlContext, calDtInputPath, calDTableName)
+    calDtDataFrame.registerTempTable(calDTableName)
+    val calDtResults = sqlContext.sql("SELECT yr_nu from CAL_DT")
+    // results.save("newcars.csv", "com.databricks.spark.csv")
+    calDtResults.map(t => "yr_nu: " + t(0)).collect().foreach(println)
+    
+
   }
+
+  def getDataHubDF(sc: SparkContext, sqlContext: SQLContext, datahubInputPath: String, tableName: String): DataFrame =
+    {
+      //Getting Schema of Datahub through Struct Type instead of Case Classes
+      val customDatahubSchema = DataHubUtil.getSchemaforDatahub();
+
+      //Creating DataFrame with header and delimiter
+      val datahubDataFrame = sqlContext.load(
+        "com.databricks.spark.csv",
+        schema = customDatahubSchema,
+        Map("path" -> datahubInputPath, "header" -> "true", "delimiter" -> "\t")).cache()
+      return datahubDataFrame;
+    }
+
+  def getCalDtDF(sc: SparkContext, sqlContext: SQLContext, datahubInputPath: String, tableName: String): DataFrame =
+    {
+      // this is used to implicitly convert an RDD to a DataFrame. (import sqlContext.implicits._). This is mandatory import when case statement is implemented
+      import sqlContext.implicits._
+
+      //Defining Input Paths : Should be parameterized
+      val calDtInputPath = "datahub/caldt"
+      val calDt = sc.textFile("datahub/caldt/CAL_DT.txt").map(_.split("\t")).map(p => CalDt(p(0), p(1), p(2), p(3), p(4), p(5), p(6), p(7), p(8), p(9))).toDF().cache()
+
+      return calDt.toDF()
+
+    }
 }
